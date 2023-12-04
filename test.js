3f4d0bb61b0cd9d7b37ebc23d3b190f8a40c6d45
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-12-04 12:13:38 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,32 @@
+// Write 15 small programming problems in a file called test.js.
+// Solve each problem and log the result to the console.
+
+// 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+// 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+// 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+// 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+// 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+// 6. Write a function that takes in three numbers and returns the average then run the function and print the result.
+
+// 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+// 8. Write a function that takes in a string and returns the first two letters. Then run the function and print the result.
+
+// 9. Write a function that takes in a number and returns the square root. Then run the function and print the result.
+
+// 10. Write a function that takes in a string and returns the last three letters. Then run the function and print the result.
+
+// 11. Write a function that takes in an array of numbers and returns the first item in the array. Then run the function and print the result.
+
+// 12. Write a function that takes in an array of numbers and returns the last item in the array. Then run the function and print the result.
+
+// 13. Write a function that takes in an array of numbers and returns true if the first and last items are the same. Then run the function and print the result.
+
+// 14. Write a function that takes in an array of numbers and returns the sum of all the numbers. Then run the function and print the result.
+
+// 15. Write a function that takes in an array of numbers and returns the numbers multiplied by two. Then run the function and print the result.
